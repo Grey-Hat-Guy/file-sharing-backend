d4d1f0a3f38fa3c174c20548e4c7c38359bc6313
Grey-Hat-Guy <greyhat2703@gmail.com>
2025-02-16 16:29:19 +0530

    Cookie settings

--- controllers/authController.js
+++ controllers/authController.js
@@ -40,7 +40,7 @@ const login = async (req, res) => {
         res.cookie('token', token, {
             httpOnly: false,
             secure: false,
-            sameSite: 'lax',
+            sameSite: 'none',
             maxAge: 60 * 60 * 1000,
         });
 
